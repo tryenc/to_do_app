715ff1b8c5986c35c580019251e155a849469c72
Tryens, Christopher <Christopher_Tryens@comcast.com>
2018-02-16 08:35:30 -0500

    Removed logic for the reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,43 +3,11 @@
 'use strict';
 
 var todos = function(state, action) {
-  state = state || [];
-  switch (action.type) {
-    case 'ADD_TODO':
-      return state.concat(
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
-      );
-    case 'TOGGLE_TODO':
-      return state.map(function(todo) {
-        if (todo.id === action.id) {
-          return Object.assign(
-            {},
-            todo,
-            {
-              completed: !todo.completed,
-            }
-          );
-        }
-        return todo;
-      });
-      // This won't work because it mutates state
-    default:
-       return state;
-  }
+  // Your code here
 };
 
 var visibilityFilter = function(state, action) {
-  state = state || 'SHOW_ALL';
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  }
+  // Your code here
 }
 
 module.exports = {
